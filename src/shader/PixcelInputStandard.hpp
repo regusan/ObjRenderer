65f81f3a660c88a25437336a07b3a75a072b860b
Ayu-WSL-Ubuntu <fake@mail.com>
2024-11-22 12:40:38 +0900

    法線データの追加と削除

--- src/shader/PixcelInputStandard.hpp
+++ src/shader/PixcelInputStandard.hpp
@@ -11,26 +11,30 @@ public:
 
     PixcelInputStandard(const VertOutputStandard &vo)
     {
-        this->positionWS = vo.positionWS;
-        this->positionCS = vo.positionCS;
         this->positionMS = vo.positionMS;
+        this->positionWS = vo.positionWS;
         this->positionPS = vo.positionPS;
+        this->positionCS = vo.positionCS;
         this->positionDS = vo.positionDS;
-        this->normalWS = vo.normalWS;
-        this->normalCS = vo.normalCS;
+
         this->normalMS = vo.normalMS;
+        this->normalWS = vo.normalWS;
+        this->normalPS = vo.normalPS;
+
         this->vertColor = vo.vertColor;
         this->uv = vo.uv;
     }
     ~PixcelInputStandard() {}
-    Vector4f positionWS = Vector4f(0, 0, 0, 1);      // ワールド基準位置
-    Vector4f positionCS = Vector4f(0, 0, 0, 1);      //  クリップ基準位置
-    Vector4f positionPS = Vector4f(0, 0, 0, 1);      //  カメラ基準位置
-    Vector4f positionMS = Vector4f(0, 0, 0, 1);      //  モデル基準位置
-    Vector4f positionDS = Vector4f(0, 0, 0, 1);      //  デバイス基準位置
-    Vector4f normalWS = Vector4f(1, 0, 0, 1);        // ワールド基準法線
-    Vector4f normalCS = Vector4f(1, 0, 0, 1);        //  カメラ基準法線
-    Vector4f normalMS = Vector4f(1, 0, 0, 1);        //  モデル基準法線
+    Vector4f positionMS = Vector4f(0, 0, 0, 1); //  モデル基準位置
+    Vector4f positionWS = Vector4f(0, 0, 0, 1); // ワールド基準位置
+    Vector4f positionPS = Vector4f(0, 0, 0, 1); //  カメラ基準位置
+    Vector4f positionCS = Vector4f(0, 0, 0, 1); // クリップ基準位置
+    Vector4f positionDS = Vector4f(0, 0, 0, 1); //  デバイス基準位置
+
+    Vector4f normalMS = Vector4f(1, 0, 0, 1); //  モデル基準法線
+    Vector4f normalWS = Vector4f(1, 0, 0, 1); // ワールド基準法線
+    Vector4f normalPS = Vector4f(1, 0, 0, 1); // カメラ基準法線
+
     Vector4f vertColor = Vector4f(255, 0, 255, 255); //  デバイス基準法線
     Vector2f uv = Vector2f(0, 0);
 };
