53d523e9f9f78c554956a4ae63ba7ad6779d3747
Ayu-WSL-Ubuntu <fake@mail.com>
2024-11-22 12:03:50 +0900

    変数の説明を付記

--- src/shader/PixcelInputStandard.hpp
+++ src/shader/PixcelInputStandard.hpp
@@ -14,7 +14,7 @@ public:
         this->positionWS = vo.positionWS;
         this->positionCS = vo.positionCS;
         this->positionMS = vo.positionMS;
-        this->positionCS = vo.positionCS;
+        this->positionPS = vo.positionPS;
         this->positionDS = vo.positionDS;
         this->normalWS = vo.normalWS;
         this->normalCS = vo.normalCS;
@@ -23,13 +23,14 @@ public:
         this->uv = vo.uv;
     }
     ~PixcelInputStandard() {}
-    Vector4f positionWS = Vector4f(0, 0, 0, 1);
-    Vector4f positionCS = Vector4f(0, 0, 0, 1);
-    Vector4f positionMS = Vector4f(0, 0, 0, 1);
-    Vector4f positionDS = Vector4f(0, 0, 0, 1);
-    Vector4f normalWS = Vector4f(1, 0, 0, 1);
-    Vector4f normalCS = Vector4f(1, 0, 0, 1);
-    Vector4f normalMS = Vector4f(1, 0, 0, 1);
-    Vector3f faceColor = Vector3f(255, 0, 255);
+    Vector4f positionWS = Vector4f(0, 0, 0, 1); // ワールド基準位置
+    Vector4f positionCS = Vector4f(0, 0, 0, 1); //  クリップ基準位置
+    Vector4f positionPS = Vector4f(0, 0, 0, 1); //  カメラ基準位置
+    Vector4f positionMS = Vector4f(0, 0, 0, 1); //  モデル基準位置
+    Vector4f positionDS = Vector4f(0, 0, 0, 1); //  デバイス基準位置
+    Vector4f normalWS = Vector4f(1, 0, 0, 1);   // ワールド基準法線
+    Vector4f normalCS = Vector4f(1, 0, 0, 1);   //  カメラ基準法線
+    Vector4f normalMS = Vector4f(1, 0, 0, 1);   //  モデル基準法線
+    Vector3f faceColor = Vector3f(255, 0, 255); //  デバイス基準法線
     Vector2f uv = Vector2f(0, 0);
 };
--- src/shader/VertOutputStandard.hpp
+++ src/shader/VertOutputStandard.hpp
@@ -11,14 +11,14 @@ private:
 public:
     VertOutputStandard(/* args */) {}
     ~VertOutputStandard() {}
-    Vector4f positionWS = Vector4f(0, 0, 0, 1);
-    Vector4f positionPS = Vector4f(0, 0, 0, 1);
-    Vector4f positionMS = Vector4f(0, 0, 0, 1);
-    Vector4f positionCS = Vector4f(0, 0, 0, 1);
-    Vector4f positionDS = Vector4f(0, 0, 0, 1);
-    Vector4f normalWS = Vector4f(1, 0, 0, 1);
-    Vector4f normalCS = Vector4f(1, 0, 0, 1);
-    Vector4f normalMS = Vector4f(1, 0, 0, 1);
-    Vector3f faceColor = Vector3f(255, 0, 255);
+    Vector4f positionWS = Vector4f(0, 0, 0, 1); // ワールド基準位置
+    Vector4f positionPS = Vector4f(0, 0, 0, 1); //  カメラ基準位置
+    Vector4f positionMS = Vector4f(0, 0, 0, 1); //  モデル基準位置
+    Vector4f positionCS = Vector4f(0, 0, 0, 1); // クリップ基準位置
+    Vector4f positionDS = Vector4f(0, 0, 0, 1); //  デバイス基準位置
+    Vector4f normalWS = Vector4f(1, 0, 0, 1);   // ワールド基準法線
+    Vector4f normalCS = Vector4f(1, 0, 0, 1);   //  カメラ基準法線
+    Vector4f normalMS = Vector4f(1, 0, 0, 1);   //  モデル基準法線
+    Vector3f faceColor = Vector3f(255, 0, 255); //  デバイス基準法線
     Vector2f uv = Vector2f(0, 0);
 };
