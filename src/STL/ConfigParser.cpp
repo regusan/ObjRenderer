5d9acccf63ad078c4c9b4dba2feaeddd367140a5
Ayu-WSL-Ubuntu <fake@mail.com>
2024-11-29 11:12:41 +0900

    コンフィグパーサーのエラーハンドリング追加

--- src/STL/ConfigParser.cpp
+++ src/STL/ConfigParser.cpp
@@ -22,35 +22,58 @@ void ConfigParser::load(string path)
 
     while (getline(infile, line))
     {
-        if (line[0] == '#') // コメントは無視
+        string nonspace = "";
+        for (char c : line)
+        {
+            if (c != ' ')
+                nonspace.push_back(c);
+        }
+
+        if (nonspace[0] == '#') // コメントは無視
             continue;
-        else if (line[0] == ' ' || line[0] == '\n' || line[0] == '\0') // 空白無視
+        else if (nonspace[0] == '\n' || nonspace[0] == '\0') // 空行や行末無視
             continue;
-        size_t idx = line.find("=");
+        size_t idx = nonspace.find("=");
         string key = "";
         string value = "";
         for (size_t i = 0; i < idx; i++)
         {
-            key.push_back(line[i]);
+            key.push_back(nonspace[i]);
         }
-        for (size_t i = idx + 1; i < line.size(); i++)
+        for (size_t i = idx + 1; i < nonspace.size(); i++)
         {
-            value.push_back(line[i]);
+            value.push_back(nonspace[i]);
         }
         data[key] = value;
     }
 }
 float ConfigParser::GetAsNumeric(string key)
 {
-    return stof(data[key]);
+    try
+    {
+        return stof(this->GetAsString(key));
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+        std::cerr << "\"" << key << "\":\"" << this->GetAsString(key) << "\"は変換不可能な値" << '\n';
+        exit(1);
+    }
 }
 string ConfigParser::GetAsString(string key)
 {
-    return data[key];
+    if (data.find(key) != data.end())
+        return data[key];
+
+    else
+    {
+        std::cerr << "Key:\"" << key << "\"は存在しないキー" << '\n';
+        exit(1);
+    }
 }
 bool ConfigParser::GetAsBool(string key)
 {
-    return (data[key] == "True") || (data[key] == "true");
+    return (this->GetAsString(key) == "True") || (this->GetAsString(key) == "true");
 }
 
 std::ostream &operator<<(std::ostream &os, const ConfigParser &cfg)
