a0fd3bdca8a0ae61c57aa83e9544dd41ad563217
Ayu-WSL-Ubuntu <fake@mail.com>
2024-12-19 14:26:50 +0900

    ライトクラスの作成

---
+++ src/Light/ILight.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "../header/EigenHeader.hpp"
+#include "../Math/Math3D.hpp"
+#include "../TransformMat.hpp"
+class PixcelInputStandard;
+using namespace std;
+
+class ILight
+{
+private:
+    /* data */
+public:
+    virtual ~ILight() = default;
+    virtual Vector3f getIlluminationAtPoint(const PixcelInputStandard &pixcelIn) const = 0;
+};
