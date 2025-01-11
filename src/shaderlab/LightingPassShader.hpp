af1f7903a103b2f22b706f0c77a541bb24a90b89
regusan-ubuntu-wsl <dammy@gmail.com>
2025-01-11 19:33:24 +0900

    LightingPasderの反射厳密化、スペキュラを0~1に正規化、AOの利用、

--- src/shaderlab/LightingPassShader.hpp
+++ src/shaderlab/LightingPassShader.hpp
@@ -14,27 +14,42 @@ using namespace Transform;
 /// @return 該当ピクセルの色
 inline const Vector3f DefferedLightingPassShader(GBuffers &gbuffers, RenderingEnvironmentParameters &environment, int x, int y)
 {
+    // 物体がなければ何もしない
+    float depthSampled = gbuffers.depth.SampleColor(x, y).x();
+    if (depthSampled == numeric_limits<float>::max())
+        return Vector3f(0, 0, 0);
+    // バッファからのサンプル結果
+    Vector3f specularSampled = gbuffers.specular.SampleColor(x, y);
+    Vector3f diffuseSampled = gbuffers.diffuse.SampleColor(x, y);
+    Vector3f normalWSSampled = gbuffers.normalWS.SampleColor(x, y);
+    Vector3f positionWSSampled = gbuffers.positionWS.SampleColor(x, y);
+    Vector3f emissionSampled = gbuffers.emission.SampleColor(x, y);
+    float aoSampled = gbuffers.AO.SampleColor(x, y).x();
+
+    Vector3f cameraPosWS = environment.viewMat.inverse().col(3).head<3>();
+
     // DirectionalLight関連の情報を取得
     Vector3f light0 = environment.directionalLights.at(0);
-    Vector3f directionalLightColor = Vector3f(1, 1, 1);
+    Vector3f directionalLightColor = Vector3f(.8, .8, .8);
 
     // 反射ベクトルの計算
-    Vector3f ref = MathPhysics::Reflect(light0, gbuffers.normalWS.SampleColor(x, y));
-    float refval = ref.dot(environment.viewMat.row(2).head<3>().normalized());
+    Vector3f ref = MathPhysics::Reflect(light0, normalWSSampled);
+    Vector3f view = (positionWSSampled - cameraPosWS).normalized();
+    float refval = ref.dot(view);
 
     // スペキュラの計算
     float specularScalar = clamp<float>(refval, 0.0, 1.0);
-    specularScalar = powf(specularScalar, 1000.0f / gbuffers.specular.SampleColor(x, y).norm());
-    Vector3f specular = gbuffers.specular.SampleColor(x, y).normalized() * specularScalar;
+    specularScalar = powf(specularScalar, 100.0f / specularSampled.norm());
+    Vector3f specular = specularSampled.normalized() * specularScalar;
 
     // DirectionalLightによるDiffuseライティング計算
-    float directIntencity = max<float>(gbuffers.normalWS.SampleColor(x, y).dot(light0), 0);
-    Vector3f directionalLight = gbuffers.diffuse.SampleColor(x, y).array() * directionalLightColor.array() * directIntencity;
+    float directIntencity = max<float>(normalWSSampled.dot(light0), 0);
+    Vector3f directionalLight = diffuseSampled.array() * directionalLightColor.array() * directIntencity * aoSampled;
 
     // アンビエントライト取得
-    Vector3f ambient = environment.ambientLight.array() * gbuffers.diffuse.SampleColor(x, y).array() * (gbuffers.depth.SampleColor(x, y).x() != numeric_limits<float>::max());
+    Vector3f ambient = environment.ambientLight.array() * diffuseSampled.array() * aoSampled;
 
     // ライト結果の合成
-    Vector3f finalColor = directionalLight + specular + ambient + gbuffers.emission.SampleColor(x, y);
+    Vector3f finalColor = directionalLight + specular + ambient + emissionSampled;
     return finalColor;
 }
