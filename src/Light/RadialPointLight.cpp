5c4ff12f234d68dd7e938f95b3858848fd8e73b1
regusan-ubuntu-wsl <dammy@gmail.com>
2025-02-09 00:06:25 +0900

    [削除]古いパンクチュアルライトシステム削除

--- src/Light/RadialPointLight.cpp
+++
@@ -1,9 +0,0 @@
-#include "RadialPointLight.hpp"
-#include "../shader/PixcelInputStandard.hpp"
-
-Vector3f RadialPointLight::getIlluminationAtPoint(const PixcelInputStandard &pixcelIn) const
-{
-    float distance = pixcelIn.positionWS.norm();
-    float illuminance = clamp<float>((this->radius - distance) / this->radius, 0, 1);
-    return this->color * illuminance;
-}
