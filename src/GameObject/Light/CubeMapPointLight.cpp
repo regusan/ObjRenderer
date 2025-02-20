f6212e0526dd67edfcc593f2907ba2946227ef7f
regusan-ubuntu-wsl <dammy@gmail.com>
2025-02-20 18:06:40 +0900

    [追加] CubeMapPointLightクラスの実装

---
+++ src/GameObject/Light/CubeMapPointLight.cpp
@@ -0,0 +1,23 @@
+#include "CubeMapPointLight.hpp"
+
+Vector3f CubeMapPointLight::lightSDF(const Vector3f &positionWS, const Vector3f &normalWS)
+{
+    if (!this->map)
+    {
+        return PointLightActor::lightSDF(positionWS, normalWS);
+    }
+    Vector3f light2PosDir = (this->location - positionWS).normalized();
+    Vector4f light2PosDir4f = Vector4f(light2PosDir.x(), light2PosDir.y(), light2PosDir.z(), 1);
+    Vector4f dirWS = Transform::ResetScale(Transform::ResetPosition(this->getMat())) * light2PosDir4f;
+    Vector2f uv = TextureMath::UVMod1(TextureMath::RectangularToPolarUV(dirWS.head<3>()));
+    return PointLightActor::lightSDF(positionWS, normalWS).array() * map->SampleColor01(uv.x(), uv.y()).array();
+}
+float CubeMapPointLight::GetBoundingSphereRadius()
+{
+    return this->radius;
+}
+CubeMapPointLight::CubeMapPointLight(json args) : PointLightActor(args)
+{
+    this->map = AssetSubSystem::getInstance().textureManager.LoadAsset(args["MapPath"]);
+}
+CubeMapPointLight::~CubeMapPointLight() {}
---
+++ src/GameObject/Light/CubeMapPointLight.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "PointLightActor.hpp"
+#include "../../header/RenderingHeader.hpp"
+using namespace Transform;
+
+/// @brief エリアライトのクラス
+class CubeMapPointLight : public PointLightActor
+{
+protected:
+    shared_ptr<Texture2D> map;
+
+public:
+    virtual Vector3f lightSDF(const Vector3f &positionWS, const Vector3f &normalWS);
+    virtual float GetBoundingSphereRadius();
+    CubeMapPointLight(json args);
+    ~CubeMapPointLight();
+};
+GCLASS(CubeMapPointLight)
