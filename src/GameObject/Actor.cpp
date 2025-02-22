0ece6c6959f8fd1e64a00d9d6f587a018eff185b
regusan-ubuntu-wsl <dammy@gmail.com>
2025-02-22 12:15:07 +0900

    [リファクタ] ActorのMatUpdateの頻度を最小化

--- src/GameObject/Actor.cpp
+++ src/GameObject/Actor.cpp
@@ -9,20 +9,24 @@ Actor::Actor(json args)
     this->SetPosition(Vector3f(positionArg[0], positionArg[1], positionArg[2]));
     this->SetRotation(Vector3f(rotationArg[0], rotationArg[1], rotationArg[2]));
     this->SetScale(Vector3f(scaleArg[0], scaleArg[1], scaleArg[2]));
+    this->matUpdate();
 }
 Actor::~Actor() {}
 
 void Actor::SetRotation(Vector3f rotate)
 {
     this->rotation = rotate;
+    this->matUpdate();
 }
 void Actor::SetPosition(Vector3f position)
 {
     this->location = position;
+    this->matUpdate();
 }
 void Actor::SetScale(Vector3f scale)
 {
     this->scale = scale;
+    this->matUpdate();
 }
 
 void Actor::matUpdate()
@@ -31,6 +35,5 @@ void Actor::matUpdate()
 }
 Matrix4f Actor::getMat()
 {
-    this->matUpdate();
     return this->mat;
 }
