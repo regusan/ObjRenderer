d053a65d506d8b098dbbd946095f51a96b24bdc3
regusan-ubuntu-wsl <dammy@gmail.com>
2025-02-22 13:26:00 +0900

    [追加] TorusLightActorクラスを実装

---
+++ src/GameObject/Light/TorusLightActor.cpp
@@ -0,0 +1,31 @@
+#include "TorusLightActor.hpp"
+
+Vector3f TorusLightActor::lightSDF(const Vector3f &positionWS, const Vector3f &normalWS)
+{
+    // 原点中心でX-Z面に円状になるトーラスで考えるため、入力posとnormをオブジェクトスペースへ変換
+    Matrix4f invModelMat = this->getMat().inverse();
+    Vector4f positionWS4 = Vector4f(positionWS.x(), positionWS.y(), positionWS.z(), 1);
+    Vector3f positionOS = (invModelMat * positionWS4).head<3>();
+    Vector3f normalOS = (invModelMat.block<3, 3>(0, 0) * normalWS).normalized(); // 3x3で位置情報を削除
+
+    Vector3f circlePos = Vector3f(positionOS.x(), 0, positionOS.z()).normalized() * this->majorRadius;
+    Vector3f dir2CirclePos = (circlePos - positionOS).normalized();
+
+    // 距離による明るさ比率
+    float dirRate = saturate(dir2CirclePos.dot(normalOS));
+    // 向きによる明るさ比率
+    float distRate = saturate(1 - (circlePos - positionOS).norm());
+    return color * distRate * dirRate;
+}
+
+float TorusLightActor::GetBoundingSphereRadius()
+{
+    return this->majorRadius + this->minorRadius;
+}
+
+TorusLightActor::TorusLightActor(json args) : LightBaseActor(args)
+{
+    this->majorRadius = args["majorRadius"];
+    this->minorRadius = args["minorRadius"];
+}
+TorusLightActor::~TorusLightActor() {}
---
+++ src/GameObject/Light/TorusLightActor.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "LightBaseActor.hpp"
+#include "../../header/RenderingHeader.hpp"
+using namespace Transform;
+
+/// @brief ポイントライトのクラス
+class TorusLightActor : public LightBaseActor
+{
+protected:
+    float majorRadius = 3.0f;
+    float minorRadius = 1.0f;
+
+public:
+    virtual Vector3f lightSDF(const Vector3f &positionWS, const Vector3f &normalWS);
+    virtual float GetBoundingSphereRadius();
+    TorusLightActor(json args);
+    TorusLightActor() {}
+    ~TorusLightActor();
+};
+GCLASS(TorusLightActor)
