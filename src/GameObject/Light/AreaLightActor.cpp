71c299ed5c7caaf117c179fac6da340c14b5042d
regusan-ubuntu-wsl <dammy@gmail.com>
2025-02-24 16:28:40 +0900

    [追加] AreaLightActorクラスを実装し、エリアライトの機能を追加

---
+++ src/GameObject/Light/AreaLightActor.cpp
@@ -0,0 +1,30 @@
+#include "AreaLightActor.hpp"
+
+Vector3f AreaLightActor::lightSDF(const Vector3f &positionWS, const Vector3f &normalWS)
+{
+    // 原点中心でX-Z面に投影するボックスライトで考えるため、入力posとnormをオブジェクトスペースへ変換
+    Matrix4f invModelMat = this->getMat().inverse();
+    Vector4f positionWS4 = Vector4f(positionWS.x(), positionWS.y(), positionWS.z(), 1);
+    Vector3f positionOS = (invModelMat * positionWS4).head<3>();
+    Vector3f normalOS = (invModelMat.block<3, 3>(0, 0) * normalWS).normalized(); // 3x3で位置情報を削除
+
+    float offset = fminf(-positionOS.y() * tan(this->barnDoorAngle / 180.0f * M_PI), this->length); // アングルに基づき拡張
+    float falloffRate = saturate(1 - fabs(positionOS.x() / (this->size.x() / 2 + offset))) *
+                        saturate(1 - fabs(positionOS.z() / (this->size.y() / 2 + offset)));
+    float distRate = saturate(1.0f - -positionOS.y() / this->length) * (positionOS.y() <= 0);
+    float normRate = saturate(normalOS.dot(Vector3f(0, 1, 0)) * 0.5 + 0.5); // half-lambert
+    return color * distRate * normRate * pow(falloffRate, this->falloff);
+}
+float AreaLightActor::GetBoundingSphereRadius()
+{
+    return this->length;
+}
+AreaLightActor::AreaLightActor(json args) : LightBaseActor(args)
+{
+    this->barnDoorAngle = args["barnDoorAngle"];
+    this->length = args["length"];
+    this->size.x() = args["size"][0];
+    this->size.y() = args["size"][1];
+    this->falloff = args["falloff"];
+}
+AreaLightActor::~AreaLightActor() {}
---
+++ src/GameObject/Light/AreaLightActor.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "LightBaseActor.hpp"
+#include "../../header/RenderingHeader.hpp"
+using namespace Transform;
+
+/// @brief エリアライトのクラス
+class AreaLightActor : public LightBaseActor
+{
+protected:
+    float length = 3.0f;
+    float barnDoorAngle = 45.0f;
+    float falloff = 0.1f;
+    Vector2f size = Vector2f(1, 1);
+
+public:
+    virtual Vector3f lightSDF(const Vector3f &positionWS, const Vector3f &normalWS);
+    virtual float GetBoundingSphereRadius();
+    AreaLightActor(json args);
+    ~AreaLightActor();
+};
+GCLASS(AreaLightActor)
