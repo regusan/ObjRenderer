749c3d072710bb9dc71c6a23ee00f76940bb2cbf
regusan-ubuntu-wsl <dammy@gmail.com>
2025-03-21 11:16:22 +0900

    [変更] FPSCameraの移動計算を修正。正しいワールドマトリックスを使用

--- src/GameObject/Camera/FPSCamera.cpp
+++ src/GameObject/Camera/FPSCamera.cpp
@@ -20,18 +20,19 @@ void FPSCamera::matUpdate()
 void FPSCamera::Tick(float deltatime)
 {
     InputSubSystem &input = InputSubSystem::getInstance();
+    this->matUpdate();
     if (input.GetKeyStatus(KeyID::W).isHeld)
-        SetLocalPosition(this->location + GetForwardVector(this->worldMatrix.inverse()).head<3>() * this->speed * deltatime);
+        SetLocalPosition(this->location + GetForwardVector(this->worldMatrix) * this->speed * deltatime);
     if (input.GetKeyStatus(KeyID::S).isHeld)
-        SetLocalPosition(this->location - GetForwardVector(this->worldMatrix.inverse()).head<3>() * this->speed * deltatime);
+        SetLocalPosition(this->location - GetForwardVector(this->worldMatrix) * this->speed * deltatime);
     if (input.GetKeyStatus(KeyID::A).isHeld)
-        SetLocalPosition(this->location - GetRightVector(this->worldMatrix.inverse()).head<3>() * this->speed * deltatime);
+        SetLocalPosition(this->location - GetRightVector(this->worldMatrix) * this->speed * deltatime);
     if (input.GetKeyStatus(KeyID::D).isHeld)
-        SetLocalPosition(this->location + GetRightVector(this->worldMatrix.inverse()).head<3>() * this->speed * deltatime);
+        SetLocalPosition(this->location + GetRightVector(this->worldMatrix) * this->speed * deltatime);
     if (input.GetKeyStatus(KeyID::E).isHeld)
-        SetLocalPosition(this->location - GetUpVector(this->worldMatrix.inverse()).head<3>() * this->speed * deltatime);
+        SetLocalPosition(this->location - GetUpVector(this->worldMatrix) * this->speed * deltatime);
     if (input.GetKeyStatus(KeyID::Q).isHeld)
-        SetLocalPosition(this->location + GetUpVector(this->worldMatrix.inverse()).head<3>() * this->speed * deltatime);
+        SetLocalPosition(this->location + GetUpVector(this->worldMatrix) * this->speed * deltatime);
 
     this->SetLocalRotation(Vector3f(-input.axisState.position.y(), -input.axisState.position.x(), 0));
 }
